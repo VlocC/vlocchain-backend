dc10da78f8c4289d8e660256ac41904d690355bc
Owen J Sullivan <multiojuice@gmail.com>
2018-09-26 22:06:37 -0700

    Preparing for new repo settup

--- README.md
+++
@@ -1,1 +0,0 @@
-VlocChain backend
--- test/index.js
+++
@@ -1,8 +0,0 @@
-import { assert } from 'chai';
-
-describe('Test', () => {
-  it('always true test', () => {
-    const expected = true;
-    assert(true, true);
-  });
-});
